76419ea58fd76ce34025f5263eebcd8ffca04045
henrythegreat3 <114631469+henrythegreat3@users.noreply.github.com>
2022-12-08 09:03:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ charactersMap.forEach((row, i) => {
           },
           scale: 3,
           animate: true,
-          dialogue: ['UWU DADDDDDDYYYY JAYYYDEN 😩']
+          dialogue: ['What the heck did you just frickin’ say about me, you little whiner? I’ll have you know I graduated top of my class in the Navy Seals,', 'and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills.', 'I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces.', 'You are nothing to me but just another target.', 'I will wipe you the frick out with precision the likes of which has never been seen before on this Earth, mark my frickin’ words.', 'You think you can get away with saying that crap to me over the Internet?', 'Think again, buddy. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, buddy.',,]
         })
-      )
+      ) 
     }
     // 1031 === oldMan
     else if (symbol === 1031) {
