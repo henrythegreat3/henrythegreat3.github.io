3cfbc4b3f53d7424d8836c69fe936400fad70533
henrythegreat3 <114631469+henrythegreat3@users.noreply.github.com>
2022-12-29 15:12:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ charactersMap.forEach((row, i) => {
           },
           scale: 3,
           animate: true,
-          dialogue: ['MERRYYYYY CHRISTMAS']   })
+          dialogue: ['hey there wedgnd']   })
       ) 
     }
     // 1031 === oldMan
